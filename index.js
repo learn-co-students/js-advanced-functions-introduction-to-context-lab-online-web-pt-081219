73cf8f69f6e75d554b347bc73c53916fa2ee90b3
Shaqqour <mshaqqourm@gmail.com>
2020-03-27 17:27:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+
+function createEmployeeRecord(record) {
+    const employee = {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee;
+}
+
+function createEmployeeRecords(employees) {
+    employees = employees.map(employee => {
+        return createEmployeeRecord(employee);
+    });
+    return employees;
+}
+
+function createTimeInEvent(employee, time) {
+    employee.timeInEvents = {
+        type: "TimeIn",
+        in: time
+    }
+    return employee;
+}
