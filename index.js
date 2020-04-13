4bcba5e94400445db911a0245caad2a69e3d4b03
Maria Le <hp.mars.le@gmail.com>
2020-04-13 16:15:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,86 +1,75 @@
 function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
 
     return {
-        firstName: firstName;
-        familyName: familyName;
-        title: title;
-        payPerHour: payPerHour;
-        timeInEvents: [];
-        timeOutEvents: [];
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
     } 
 }
 
-function createEmployeeRecords(arrArrays) {
-    let arrObjects = arrArrays.map(createEmployeeRecord);
-    return arrObjects;
+function createEmployeeRecords(records) {
+    let empRecords = records.map(createEmployeeRecord);
+    return empRecords;
 }
 
-/*
-Argument(s)
-Array of Arrays
-Returns
-Array of Objects
-Behavior
-Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new Array
-createTimeInEvent
-Argument(s)
-An employee record Object
-A date stamp ("YYYY-MM-DD HHMM")
-Returns
-The employee record
-Behavior
-Add an Object with keys to the timeInEvents Array on the record Object:
-type: Set to "TimeIn"
-hour: Derived from the argument
-date: Derived from the argument
+function createTimeInEvent(empRecord, dateStamp) {
+    let h = parseInt(dateStamp.slice(11));
+    let d = dateStamp.slice(0,10);
+    empRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: h,
+        date: d
+    });
+    return empRecord;
+}
 
-createTimeOutEvent
-Argument(s)
-An employee record Object
-A date stamp ("YYYY-MM-DD HHMM")
-Returns
-The employee record
-Behavior
-Add an Object with keys to the timeOutEvents Array on the record Object:
-type: Set to "TimeOut"
-hour: Derived from the argument
-date: Derived from the argument
-hoursWorkedOnDate
-Argument(s)
-An employee record Object
-A date of the form "YYYY-MM-DD"
-Returns
-Hours worked, an Integer
-Behavior
-Given a date, find the number of hours elapsed between that date's timeInEvent and timeOutEvent
-wagesEarnedOnDate
-Argument(s)
-An employee record Object
-A date of the form "YYYY-MM-DD"
-Returns
-Pay owed
-Behavior
-Using hoursWorkedOnDate, multiply the hours by the record's payRate to determine amount owed. Amount should be returned as a number.
-allWagesFor
-Argument(s)
-An employee record Object
-Returns
-Pay owed for all dates
-Behavior
-Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number. HINT: You will need to find the available dates somehow...
-findEmployeeByFirstName
-Argument(s)
-srcArray: Array of employee records
-firstName: String representing a first name held in an employee record
-Returns
-Matching record or undefined
-Behavior
-Test the firstName field for a match with the firstName argument
-calculatePayroll
-Argument(s)
-Array of employee records
-Returns
-Sum of pay owed to all employees for all dates, as a number
-Behavior
-Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number.
-*/
+function createTimeOutEvent(empRecord, dateStamp) {
+    let h = parseInt(dateStamp.slice(11));
+    let d = dateStamp.slice(0,10);
+    empRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: h,
+        date: d
+    });
+    return empRecord;
+}
+
+function hoursWorkedOnDate(record, date) {
+    //find timeInEvent for the date
+    let inTime = record.timeInEvents.find((e) => e.date === date) 
+    //find timeOutEvent for the date
+    let outTime = record.timeOutEvents.find((e) => e.date === date)
+    //find hours worked by subtracting time in from time out
+    let hoursWorked = (outTime.hour - inTime.hour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(record, date){
+    let pay = hoursWorkedOnDate(record, date) * record.payPerHour
+    return pay
+}
+
+function allWagesFor(record){
+    //find all dates worked
+    let workedDates = record.timeInEvents.map((e) => e.date)
+    //find wages earned for all dates
+    let wages = workedDates.map((d) => wagesEarnedOnDate(record, d))
+    //add wages together
+    let payAlldates = wages.reduce((total, e) => total + e)
+    
+    return payAlldates
+}
+
+function findEmployeeByFirstName(records, firstName) {
+    const empRecord = records.find((r) => r.firstName === firstName)
+    return empRecord
+}
+
+function calculatePayroll(records) {
+    let allPay = records.map(allWagesFor);
+    allPay = allPay.reduce((total, p) => total + p)
+    return allPay
+}
